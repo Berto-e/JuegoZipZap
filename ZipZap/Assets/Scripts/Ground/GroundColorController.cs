b02eb12507ce7aa2dd86eb41347ec862a4022080
Berto-e <65825564+Berto-e@users.noreply.github.com>
2023-03-01 04:06:22 +0100

    Update ZipZap
    He añadido la generación de terreno nuevo  y nuevos Scripts para el control del terreno

--- ZipZap/Assets/Scripts/Ground/GroundColorController.cs
+++ ZipZap/Assets/Scripts/Ground/GroundColorController.cs
@@ -6,5 +6,46 @@ public class GroundColorController : MonoBehaviour
 {
    [SerializeField] private Material groundMaterial;
    [SerializeField] private Color[] colors;
-   private int coloIndex = 0;
+   private int colorIndex = 0;
+   [SerializeField] private float lerpValue;
+   [SerializeField] private float time;
+   private float currentTime;
+
+   private void Update()
+   {
+        SetColorChangeTime();
+        SetGroundMaterialSmoothColorChange();
+   }
+
+   private void SetColorChangeTime()
+   {
+        if(currentTime <= 0)
+        {
+                CheckColorIndexValue();
+                currentTime = time;
+        }
+        else
+        {
+                currentTime -= Time.deltaTime;
+        }
+   }
+
+   private void CheckColorIndexValue()
+   {
+    colorIndex++;
+    if(colorIndex >= colors.Length)
+    {
+        colorIndex = 0;
+    }
+   }
+
+   private void SetGroundMaterialSmoothColorChange()
+   {
+    groundMaterial.color = Color.Lerp(groundMaterial.color, colors[colorIndex], lerpValue* Time.deltaTime);
+   }
+
+   private void OnDestroy() 
+   {
+     groundMaterial.color = colors[1];
+   }
 }
